76b31db085878de0ea6b366e571248c5fa97a28c
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-16 17:29:15 -0400

    Initial Project setup

---
+++ Depressed-FromJrToSr/Pods/Target Support Files/Pods-Depressed-FromJrToSr/Pods-Depressed-FromJrToSr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Depressed_FromJrToSr : NSObject
+@end
+@implementation PodsDummy_Pods_Depressed_FromJrToSr
+@end
---
+++ Depressed-FromJrToSr/Pods/Target Support Files/Pods-Depressed-FromJrToSrTests/Pods-Depressed-FromJrToSrTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Depressed_FromJrToSrTests : NSObject
+@end
+@implementation PodsDummy_Pods_Depressed_FromJrToSrTests
+@end
