52f5ca543aeb9bf5c3d47bc52a86182a1507c922
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-16 13:36:59 -0400

    Moved Timer to respectful component, Initialized app from AppDelegate

--- HexClock/HexClock/Controller/HexClockViewController.swift
+++ HexClock/HexClock/Controller/HexClockViewController.swift
@@ -9,13 +9,32 @@ import UIKit
 
 /// Hex Clock view controller.
 class HexClockViewController: UIViewController {
-
-	// MARK: UIViewController
-
-	override func viewDidLoad() {
-		let clockView = HexClockView()
-		view.addSubview(clockView)
-		clockView.pinTo(view)
+	
+	private let clockView = HexClockView()
+	
+	private lazy var timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+		guard let self = self else { return }
+		
+		// Formatting
+		let now = Date()
+		let (hour, minute, second) = now.timeComponents
+		let hourString = String(format: "%02d", hour)
+		let minuteString = String(format: "%02d", minute)
+		let secondString = String(format: "%02d", second)
+		
+		// Set the Hex value for the clock
+		self.clockView.clockLabel.text = "#" + hourString + minuteString + secondString
+		
+		// Animate the background color
+		UIView.animate(withDuration: 1.0) {
+			self.clockView.backgroundColor = UIColor.fromTime(hour: hour, minute: minute, second: second)
+		}
 	}
 
+	// MARK: UIViewController
+	override func viewDidLoad() {
+		view.addSubview(clockView)
+		clockView.pinTo(view)
+		timer.fire()
+	}
 }
--- HexClock/HexClock/View/Custom Views/HexClockView.swift
+++ HexClock/HexClock/View/Custom Views/HexClockView.swift
@@ -11,33 +11,14 @@ import UIKit
 final class HexClockView: UIView {
 	
 	// MARK: Properties
-	private var clockLabel: UILabel = {
+	let clockLabel: UILabel = {
 		let clockLabel = UILabel()
 		clockLabel.translatesAutoresizingMaskIntoConstraints = false
-		clockLabel.textColor = UIColor.white
+		clockLabel.textColor = .white
 		clockLabel.textAlignment = .center
 		clockLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 250, weight: .ultraLight)
 		return clockLabel
 	}()
-	
-	private lazy var timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
-		guard let self = self else { return }
-		let now = Date()
-		let (hour, minute, second) = now.timeComponents
-		let hourString = String(format: "%02d", hour)
-		let minuteString = String(format: "%02d", minute)
-		let secondString = String(format: "%02d", second)
-		
-		// Set the Hex value for the clock
-		self.clockLabel.text = "#" + hourString + minuteString + secondString
-		
-		// Animate the background color
-		UIView.animate(withDuration: 1.0) {
-			self.backgroundColor = UIColor.fromTime(hour: hour, minute: minute, second: second)
-		}
-	}
-	
-	
 	
 	// MARK: Initialization
 	init() {
